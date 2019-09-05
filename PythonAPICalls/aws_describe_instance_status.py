7b17555b188eb885c4d354d612b189d0f2e07dfd
Dan Cave <wanderdog00@gmail.com>
2019-09-05 21:58:25 +0100

    Tinkered with my AWS JSON project

--- PythonAPICalls/aws_describe_instance_status.py
+++ PythonAPICalls/aws_describe_instance_status.py
@@ -5,5 +5,9 @@ ec2 = boto3.client('ec2')
 
 response = ec2.describe_instance_status(Filters=[{'Name' : 'event.code','Values' : ['instance-reboot','system-reboot','system-maintenance','instance-retirement','instance-stop']}])
 
-print(response)
+#print(response)
 
+print(response['InstanceStatuses'])
+
+for responses in response['InstanceStatuses']:
+    responses 
